6374a563b406c424298d381980c92ae652b3d839
chris0076 <crcoll9955@northgeorgia.edu>
2013-03-18 22:42:32 -0400

    Initial commit

---
+++ mandelbrot.js
@@ -0,0 +1,142 @@
+function insideMandelbrot(x, y) {
+    var p = p = Math.sqrt((Math.pow(x - .25, 2) + y*y));
+    return (x < p - Math.pow(2.0 * p, 2) + .25) || (Math.pow(x + 1.0, 2) + (y*y) < 0.0625);
+}
+
+function mandelbrot(cr, ci, iterations, smooth) {
+    var count = 0;
+    var zr = 0.0;
+    var zi = 0.0;
+    if (insideMandelbrot(cr, ci)) {
+        count = iterations;
+    }
+    while ((count < iterations) && ((zr * zr + zi *zi) < 4.0 )) {
+        var temp = zr * zr - zi * zi + cr;
+        zi = 2 * zr * zi + ci;
+        zr = temp;
+        count++;
+    }
+    if (!smooth)
+        return count;
+    var colorindex = 0;
+    if (count < iterations) {
+        var mu = (Math.log(Math.log((zr*zr + zi*zi)))) / Math.log(2.0);
+        colorindex = Math.floor((count - mu) * iterations / smooth);
+        if (colorindex < 0) colorindex = 0;
+        return colorindex % (iterations*2);
+    }
+    return colorindex;
+}
+
+function mandelbrotPath(cr, ci, iterations) {
+    var zr = 0.0, zi = 0.0;
+    var count = 0;
+
+    points = []
+    if (mandelbrot(cr, ci, iterations, 0) != iterations) {
+        for (;count < iterations;count++) {
+            if ((zr*zr + zi*zi) <= 4.0) {
+                var temp = zr * zr - zi * zi + cr;
+                zi = 2 * zr * zi + ci;
+                zr = temp;
+                points.push([zr, zi]);
+            } else {
+                break;
+            }
+        }
+    }
+    return points;
+}
+
+
+function drawMandelbrot(image, iterations, smooth) {
+
+    var pixels = image.data;
+    var minx = -2.0;
+    var maxx = 1.0;
+    var miny = -1.0;
+    var maxy = 1.0;
+
+    var pitchx = (maxx - minx) / image.width;
+    var pitchy = (maxy - miny) / image.height;
+
+    for (var i = 0; i < image.width; i++) {
+        for (var j = 0; j < image.height; j++) {
+            var val = mandelbrot(i*pitchx+minx, j*pitchy+miny, iterations, smooth) / iterations * 255;
+            var idx = image.width * j + i;
+            pixels[idx * 4] = val;
+            pixels[idx * 4 + 1] = val;
+            pixels[idx * 4 + 2] = val;
+            pixels[idx * 4 + 3] = 255;
+        }
+    }
+}
+
+$(document).ready(function () {
+    var points = [];
+    var canvas = document.getElementById("mandelbrot");
+    var ctx = canvas.getContext("2d");
+    var imageData = ctx.createImageData(canvas.width, canvas.height);
+
+    ctx.font = "bold 16px Arial";
+    ctx.textalign = "center";
+    ctx.fillText("CLICK HERE", 300, 200)
+    ;
+
+    var itervals = [1, 2, 3, 4, 5, 10, 15, 20, 30, 40, 50, 100, 150, 200, 255];
+    var num = 0;
+
+    $(canvas).click(function(e) {
+        iterations = itervals[num];
+        drawMandelbrot(imageData, iterations);
+        ctx.putImageData(imageData, 0, 0);
+        num = (num + 1) % itervals.length;
+    });
+});
+
+$(document).ready(function () {
+    var points = [];
+    var canvas = document.getElementById("mandelbrotsmooth");
+    var ctx = canvas.getContext("2d");
+    var imageData = ctx.createImageData(canvas.width, canvas.height);
+
+    var iterations = 5;
+    drawMandelbrot(imageData, 255, 10);
+    ctx.putImageData(imageData, 0, 0);
+});
+
+$(document).ready(function () {
+    var points = [];
+    var canvas = document.getElementById("mandelbrotpath");
+    var ctx = canvas.getContext("2d");
+    ctx.font = "bold 16px Arial";
+    ctx.textalign = "center";
+    ctx.fillText("CLICK HERE", canvas.width/2, canvas.height/2);
+
+    $(canvas).click(function (e) {
+        var iterations = 255
+        var imageData = ctx.createImageData(canvas.width, canvas.height);
+        drawMandelbrot(imageData, iterations, 0)
+        ctx.putImageData(imageData, 0, 0);
+
+        var minx = -2.0;
+        var maxx = 1.0;
+        var miny = -1.0;
+        var maxy = 1.0;
+        var pitchx = (maxx - minx) / canvas.width;
+        var pitchy = (maxy - miny) / canvas.height;
+        points = mandelbrotPath(e.offsetX*pitchx+minx, e.offsetY*pitchy+miny, iterations);
+
+        ctx.strokeStyle = "#00FF00";
+        ctx.beginPath();
+        if (points) {
+            ctx.moveTo((points[0][0]-minx)/pitchx, (points[0][1]-miny)/pitchy);
+            for (var i = 1; i < points.length; i++) {
+                ctx.strokeStyle = "rgb(0," + (iterations - i) + ", 0)";
+                ctx.lineTo((points[i][0]-minx)/pitchx, (points[i][1]-miny)/pitchy);
+            }
+            ctx.stroke();
+        }
+    });
+
+});
---
+++ voronoi.js
@@ -0,0 +1,115 @@
+function random(seed) {
+    var x = Math.sin(seed) * 23443.23;
+    return x - Math.floor(x);
+}
+
+function euclidean2(dx, dy) {
+    return dx * dx + dy * dy;
+}
+function euclidean(dx, dy) {
+    return Math.sqrt(euclidean2(dx, dy));
+}
+function manhattan(dx, dy) {
+    return Math.abs(dx) + Math.abs(dy);
+}
+function chebychev(dx, dy) {
+    return Math.max(Math.abs(dx), Math.abs(dy));
+}
+
+
+
+function voronoi(x, y) {
+    var px = Math.floor(x);
+    var py = Math.floor(y);
+
+    var fx = x - Math.floor(x);
+    var fy = y - Math.floor(y);
+
+    var res = 8.0;
+    for (var j = -1; j <= 1; j++) {
+        for (var i = -1; i <= 1; i++) {
+            var rx = i - fx + random((i + px) + (j + py) * 100);
+            var ry = j - fy + random((i + px) + (j + py) * 1000);
+
+            res = Math.min(res, rx * rx + ry * ry);
+        }
+    }
+    return Math.sqrt(res);
+}
+
+function voronoiPoints(points, x, y) {
+    var px = Math.floor(x);
+    var py = Math.floor(y);
+
+    var fx = x - Math.floor(x);
+    var fy = y - Math.floor(y);
+
+    var res = 8.0;
+    for (var i = 0; i < points.length; i++) {
+        var rx = x - points[i].x;
+        var ry = y - points[i].y;
+
+
+        res = Math.min(res, euclidean2(rx, ry));
+    }
+    return Math.sqrt(res);
+}
+
+
+function drawVoronoi(image, points, scale) {
+    var offset = Math.random() * 100;
+
+    var data = [];
+    var max = 0.0,
+        min = 1e9;
+    for (var j = 0; j < image.height; j++) {
+        for (var i = 0; i < image.width; i++) {
+            //var temp = voronoi(i * scale + offset, j * scale + offset);
+            var temp = voronoiPoints(points, i * scale, j * scale);
+            if (temp < min) {
+                min = temp;
+            }
+            if (temp > max) {
+                max = temp;
+            }
+            data[image.width * j + i] = temp;
+        }
+    }
+
+    var pixels = image.data;
+    for (var i = 0; i < image.width * image.height; i++) {
+        var val = ((data[i] - min) / max) * 255 & 0xff;
+        pixels[i * 4] = val;
+        pixels[i * 4 + 1] = val;
+        pixels[i * 4 + 2] = val;
+        pixels[i * 4 + 3] = 255;
+    }
+}
+
+$(document).ready(function () {
+    var points = [];
+    var canvas = document.getElementById("voronoi");
+    var ctx = canvas.getContext("2d");
+    ctx.font = "bold 16px Arial";
+    ctx.textalign = "center";
+    ctx.fillText("CLICK HERE", canvas.width/2, canvas.height/2);
+
+    $(canvas).click(function (e) {
+        var imageData = ctx.createImageData(canvas.width, canvas.height);
+
+        var scale = 1 / 64;
+        var p = {};
+        p.x = e.offsetX * scale;
+        p.y = e.offsetY * scale;
+        points.push(p);
+        drawVoronoi(imageData, points, scale);
+        ctx.putImageData(imageData, 0, 0);
+
+        ctx.fillStyle = "#00FF00";
+        for (var i = 0; i < points.length; i++) {
+            ctx.arc(points[i].x / scale, points[i].y / scale, 2, 0, 2 * Math.PI);
+            ctx.fill();
+        }
+    });
+
+});
