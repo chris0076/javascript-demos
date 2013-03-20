fa75ed12f3ef42d7d18e64ffa027c7850afaf637
chris0076 <crcoll9955@northgeorgia.edu>
2013-03-19 23:14:08 -0400

    Add buddhabrot demo

--- mandelbrot.js
+++ mandelbrot.js
@@ -28,11 +28,15 @@ function mandelbrot(cr, ci, iterations, smooth) {
     return colorindex;
 }
 
-function mandelbrotPath(cr, ci, iterations) {
+function mandelbrotPath(cr, ci, iterations, optimize) {
     var zr = 0.0, zi = 0.0;
     var count = 0;
 
     points = []
+
+    if ((mandelbrot(cr, ci, iterations, 0) == iterations) && optimize) {
+        return [];
+    }
 
     for (;count < iterations;count++) {
         if ((zr*zr + zi*zi) <= 4.0) {
@@ -48,6 +52,53 @@ function mandelbrotPath(cr, ci, iterations) {
     return points;
 }
 
+function lerp(a,b,x) { return a + x * (b-a);}
+
+function sampleBuddhbrot(image, array, iterations, samples) {
+    var minx = -2.0;
+    var maxx = 1.0;
+    var miny = -1.0;
+    var maxy = 1.0;
+
+    var pitchx = (maxx - minx) / image.width;
+    var pitchy = (maxy - miny) / image.height;
+
+    for (var i = 0; i < samples; i++) {
+        x = lerp(minx, maxx, Math.random());
+        y = lerp(miny, maxy, Math.random());
+        var points = mandelbrotPath(x, y, iterations, 1);
+
+        for (var j = 0; j < points.length; j++) {
+            vx = Math.floor((points[j][0] - minx) / pitchx);
+            vy = Math.floor((points[j][1] - miny) / pitchy);
+
+
+            if ((vx >= 0) && (vy >= 0) && (vx < image.width) && (vy < image.height)) {
+                array[image.width*vy + vx] += 1;
+            }
+        }
+    }
+    return array
+}
+
+function drawBuddhabrot(image, array) {
+    var pixels = image.data;
+    var min = Infinity;
+    var max = 0;
+
+    for (var i = 0; i<array.length; i++) {
+        if (array[i] < min) { min = array[i]; }
+        if (array[i] > max) { max = array[i]; }
+    }
+
+    for (var i=0; i < array.length; i++) {
+        var val = ((array[i] - min) / max) * 255;
+        pixels[i * 4] = val;
+        pixels[i * 4 + 1] = val;
+        pixels[i * 4 + 2] = val;
+        pixels[i * 4 + 3] = 255;
+    }
+}
 
 function drawMandelbrot(image, iterations, smooth) {
 
@@ -80,13 +131,12 @@ $(document).ready(function () {
 
     ctx.font = "bold 16px Arial";
     ctx.textalign = "center";
-    ctx.fillText("CLICK HERE", 300, 200)
-    ;
+    ctx.fillText("CLICK HERE", 300, 200);
 
     var itervals = [1, 2, 3, 4, 5, 10, 15, 20, 30, 40, 50, 100, 150, 200, 255];
     var num = 0;
 
-    $(canvas).click(function(e) {
+    $(canvas).click(function (e) {
         iterations = itervals[num];
         drawMandelbrot(imageData, iterations);
         ctx.putImageData(imageData, 0, 0);
@@ -140,4 +190,26 @@ $(document).ready(function () {
         }
     });
 
+});
+
+$(document).ready(function () {
+    var points = [];
+    var canvas = document.getElementById("buddhabrot");
+    var ctx = canvas.getContext("2d");
+    var imageData = ctx.createImageData(canvas.width, canvas.height);
+
+    ctx.font = "bold 16px Arial";
+    ctx.textalign = "center";
+    ctx.fillText("CLICK HERE", 300, 200);
+
+
+    var iterations = 255;
+    var array = []
+    for (var i = 0; i < canvas.width*canvas.height; i++) {array.push(0); }
+
+    $(canvas).click(function (e) {
+        array = sampleBuddhbrot(imageData, array, iterations, 100000);
+        drawBuddhabrot(imageData, array);
+        ctx.putImageData(imageData, 0, 0);
+    });
 });
