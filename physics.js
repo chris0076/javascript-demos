58690bff87ea2b9aff10608fa1adf7df6d6cd8c5
chris0076 <crcoll9955@northgeorgia.edu>
2013-03-31 01:15:03 -0400

    Update physics.js to use the new vector stuff

--- physics.js
+++ physics.js
@@ -1,42 +1,33 @@
 $(document).ready(function () {
-    function Planet(px, py, vx, vy, m, r, color) {
+    function Planet(position, velocity, m, r, color) {
         this.m = m;
         this.r = r;
         this.color = color;
 
-        this.px = px;
-        this.py = py;
+        this.position = new Vector(position);
+        this.velocity = new Vector(velocity);
 
-        this.vx = vx;
-        this.vy = vy;
-
-        this.points = []
+        this.points = [];
 
         this.update = function (bodies, dt) {
             // F = G * m1 * m2 / r^2;
             // F/m = a
-            this.points.push([this.px, this.py]);
+            this.points.push(this.position.comp);
 
-            var ax = 0;
-            var ay = 0;
+            var accel = new Vector(0,0);
+
             for (var i=0; i<bodies.length;i++) {
                 body = bodies[i];
 
-                var dx = body.px-this.px;
-                var dy = body.py-this.py;
-
-                var mag2 = Math.pow(dx, 2) + Math.pow(dy, 2);
-                var mag = Math.sqrt(mag2);
+                var dp = body.position.sub(this.position);
+                var mag2 = dp.magnitude2();
                 var temp = 500*body.m*this.m/mag2;
-                ax += temp*dx;
-                ay += temp*dy;
+                accel.iadd(dp.mul(temp));
             }
 
-            this.px += this.vx * dt;
-            this.vx += ax * dt / this.m;
+            this.position.iadd(this.velocity.mul(dt));
+            this.velocity.iadd(accel.mul(dt / this.m));
 
-            this.py += this.vy * dt;
-            this.vy += ay * dt / this.m;
         };
         this.render = function (ctx) {
             ctx.save();
@@ -50,7 +41,7 @@ $(document).ready(function () {
 
             ctx.beginPath();
             ctx.fillStyle = this.color;
-            ctx.arc(this.px, this.py, this.r, 0, 2 * Math.PI);
+            ctx.arc(this.position.comp[0], this.position.comp[1], this.r, 0, 2 * Math.PI);
             ctx.fill();
             ctx.restore();
         }
@@ -59,9 +50,8 @@ $(document).ready(function () {
     var canvas = document.getElementById("euler");
     var ctx = canvas.getContext("2d");
 
-
-    var planet = new Planet(canvas.width/4, canvas.height/2, 0, 450,   1, 1, "#00FF66");
-    var sun    = new Planet(canvas.width/2, canvas.height/2, 0,   0, 500, 5, "#FFBB00");
+    var planet = new Planet([canvas.width/4, canvas.height/2], [0, 450],   1, 1, "#00FF66");
+    var sun    = new Planet([canvas.width/2, canvas.height/2], [0,   0], 500, 5, "#FFBB00");
 
     $(canvas).hover(function (e) {
         iid = setInterval(function() {
