15fb14fa39017c0e7151fdf061becafdd4b2095c
chris0076 <crcoll9955@northgeorgia.edu>
2013-03-31 23:47:05 -0400

    Modularize update function

--- physics.js
+++ physics.js
@@ -1,8 +1,16 @@
+function euler(bodies, dt) {
+    var accel = this.accelerate(bodies, dt);
+    this.position.iadd(this.velocity.mul(dt));
+    this.velocity.iadd(accel.mul(dt / this.m));
+    this.points.push(this.position.comp);
+}
+
 $(document).ready(function () {
-    function Planet(position, velocity, m, r, color) {
+    function Planet(position, velocity, m, r, color, update) {
         this.m = m;
         this.r = r;
         this.color = color;
+        this.update = update;
 
         this.position = new Vector(position);
         this.velocity = new Vector(velocity);
@@ -20,17 +28,7 @@ $(document).ready(function () {
             }
             return accel;
         };
-        this.update = function (bodies, dt) {
-            // F = G * m1 * m2 / r^2;
-            // F/m = a
 
-            var accel = accelerate(bodies, dt);
-
-            this.position.iadd(this.velocity.mul(dt));
-            this.velocity.iadd(accel.mul(dt / this.m));
-            this.points.push(this.position.comp);
-
-        };
         this.render = function (ctx) {
             ctx.save();
             ctx.lineWidth = .5;
@@ -52,8 +50,8 @@ $(document).ready(function () {
     var canvas = document.getElementById("euler");
     var ctx = canvas.getContext("2d");
 
-    var planet = new Planet([canvas.width/4, canvas.height/2], [0, 450],   1, 1, "#00FF66");
-    var sun    = new Planet([canvas.width/2, canvas.height/2], [0,   0], 500, 5, "#FFBB00");
+    var planet = new Planet([canvas.width/4, canvas.height/2], [0, 450],   1, 1, "#00FF66", euler);
+    var sun    = new Planet([canvas.width/2, canvas.height/2], [0,   0], 500, 5, "#FFBB00", euler);
 
     $(canvas).hover(function (e) {
         iid = setInterval(function() {
